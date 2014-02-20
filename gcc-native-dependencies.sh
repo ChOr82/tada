c858eca557bc3dbbdc0672055497cb4f0772336c
Rego <pvrego@riseup.net>
2014-02-19 21:53:24 -0300

    Update script.

--- gcc-native-dependencies.sh
+++ gcc-native-dependencies.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
 cd /opt/tada/sources
+curr_lib="none"
 
-cd m4-1.4.17
-./configure --prefix=/opt/tada/gcc-native
-make && make check && make install
+echo "Installation from gcc dependencies will start for libraries"
+echo "==========================================================="
 
-cd ../gmp-5.1.3
-./configure --prefix=/opt/tada/gcc-native
-make && make check && make install
+curr_lib="m4-1.4.17"
+echo "Install $curr_lib. Continue? (y/n)"
+read user_option
 
-cd ../mpfr-3.1.2
-./configure --prefix=/opt/tada/gcc-native --with-gmp=/opt/tada/gcc-native
-make && make check && make install
+if ("$user_option" == "y") then
+	cd $curr_lib
+	./configure --prefix=/opt/tada/gcc-native
+	make && make check && make install
+else
+	echo "Ok. Finished."
+	exit
+fi
 
-cd ../mpc-1.0.2
-./configure --prefix=/opt/tada/gcc-native
-make && make check && make install
+#cd ../gmp-5.1.3
+#./configure --prefix=/opt/tada/gcc-native
+#make && make check && make install
 
-cd ../automake-1.9
-./configure --prefix=/opt/tada/gcc-native
-make && make check && make install
+#cd ../mpfr-3.1.2
+#./configure --prefix=/opt/tada/gcc-native --with-gmp=/opt/tada/gcc-native
+#make && make check && make install
+
+#cd ../mpc-1.0.2
+#./configure --prefix=/opt/tada/gcc-native
+#make && make check && make install
+
+#cd ../automake-1.9
+#./configure --prefix=/opt/tada/gcc-native
+#make && make check && make install
 	
-cd ../autoconf-2.69
-./configure --prefix=/opt/tada/gcc-native
-make && make check && make install
+#cd ../autoconf-2.69
+#./configure --prefix=/opt/tada/gcc-native
+#make && make check && make install
 
 #cd ../flex-2.5.37
 #./configure --prefix=/opt/tada/gcc-native
